c4fbb7a4f2fca8aaf1968ba6f9e52ecbf6f59589
tbrekalo <brekalo.tvrtko@gmail.com>
2020-12-21 17:39:04 +0100

    inital commit

---
+++ include/vanna/allocator.hpp
@@ -0,0 +1,13 @@
+#ifndef VANNA_ALLOCATOR_HPP_
+#define VANNA_ALLOCATOR_HPP_
+
+namespace vanna {
+
+template <class T> class allocator {
+public:
+private:
+};
+
+} // namespace vanna
+
+#endif /* VANNA_ALLOCATOR_HPP_ */
---
+++ include/vanna/memory_resource.hpp
@@ -0,0 +1,13 @@
+#ifndef VANNA_MEMORY_RESOURCE_HPP_
+#define VANNA_MEMORY_RESOURCE_HPP_
+
+namespace vanna {
+
+class memory_resource {
+public:
+private:
+};
+
+}
+
+#endif /* VANNA_MEMORY_RESOURCE_HPP_ */
